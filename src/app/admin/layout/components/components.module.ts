0cef5e989dc3fa174e1e92e1752e4683e700f6c0
huseyin corakli <huseyincorakli@icloud.com>
2023-07-15 09:14:54 +0300

    Angular Material

--- src/app/admin/layout/components/components.module.ts
+++ src/app/admin/layout/components/components.module.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterModule } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
 
 
 
@@ -14,7 +15,7 @@ import { RouterModule } from '@angular/router';
     FooterComponent
   ],
   imports: [
-    CommonModule,RouterModule
+    CommonModule,RouterModule,MatListModule
   ],exports:[
     HeaderComponent,
     SidebarComponent,
