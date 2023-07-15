4c04e13d7834d7d6bc94dede7eb02ad37070da6b
huseyin corakli <huseyincorakli@icloud.com>
2023-07-15 12:04:09 +0300

    Bootstrap Jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+// declare var $:any;
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ETicaretClient_2023';
+  
 }
+
