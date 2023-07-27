629f4a7ed8950c380badbe74f2a5cc88e462219d
huseyin corakli <huseyincorakli@icloud.com>
2023-07-28 02:54:46 +0300

    fix

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,9 +1,12 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Directive, ElementRef, EventEmitter, HostListener, Input, Output, Renderer2 } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
-import { ProductService } from 'src/app/services/common/models/product.service';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
+
 declare var $:any;
 @Directive({
   selector: '[appDelete]'
@@ -13,9 +16,10 @@ export class DeleteDirective {
   constructor(
     private element: ElementRef,
     private _render: Renderer2,
-    private productService: ProductService,
+    private httpClientService: HttpClientService,
     private spinner:NgxSpinnerService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private alertify:AlertifyService
   ) { 
 
     const img= _render.createElement('img');
@@ -27,20 +31,37 @@ export class DeleteDirective {
   }
   @Output() callback:EventEmitter<any>= new EventEmitter();
   @Input() id:string;
+  @Input() controller:string;
   @HostListener('click')
   async onclick(){
     this.openDialog(async ()=>{
       this.spinner.show(SpinnerType.Classic)
       const td=HTMLTableCellElement=this.element.nativeElement
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate({
-        opacity:0,
-        left:'+=50',
-        height:'toogle'
-      },800,()=>{
-        this.callback.emit()
+      this.httpClientService.delete({
+        controller:this.controller
+      },this.id).subscribe(data=>{
+        $(td.parentElement).animate({
+          opacity:0,
+          left:'+=50',
+          height:'toogle'
+        },800,()=>{
+          this.callback.emit()
+        })
+        this.spinner.hide(SpinnerType.Classic)
+        this.alertify.message('Silme işlemi başarılı',{
+          dismissOthers:true,
+          messageType:MessageType.Success,
+          position:Position.BottomRight
+        })
+      },(errorResponse:HttpErrorResponse)=>{
+        this.spinner.hide(SpinnerType.Classic)
+        this.alertify.message(errorResponse.message,{
+          dismissOthers:true,
+          messageType:MessageType.Error,
+          position:Position.BottomRight
+        })
       })
-      this.spinner.hide(SpinnerType.Classic)
+      
     })
   }
 
