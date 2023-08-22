9f97afe6b36610088646a8e1a9b4e88489265b16
huseyin corakli <huseyincorakli@icloud.com>
2023-08-22 03:34:52 +0300

    jwt

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,13 +3,15 @@ import { HttpClientService } from '../http-client.service';
 import { User } from 'src/app/entities/user';
 import { Create_User } from 'src/app/contracts/users/create_user';
 import { firstValueFrom,Observable } from 'rxjs';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService: HttpClientService) { }
+  constructor(private httpClientService: HttpClientService,private toastr:CustomToastrService) { }
 
  async create(user:User):Promise<Create_User>{
   const observable: Observable<User | Create_User>= this.httpClientService.post<Create_User |User>({
@@ -19,12 +21,17 @@ export class UserService {
   }
 
   async login(usernameOrEmail:string,password:string,callBackFunction?:()=> void):Promise<void>{
-    const observable:Observable<any> = this.httpClientService.post({
+    const observable:Observable<any> = this.httpClientService.post<any|TokenResponse>({
       controller:'users',
       action:'login',
     },{usernameOrEmail,password})
     
-    await firstValueFrom(observable)
+    const tokenResponse:TokenResponse=  await firstValueFrom(observable) as TokenResponse;
+    
+    if (tokenResponse) {
+      this.toastr.message('Başarılı',"Kullanıcı Girişi Başarılı",ToastrMessageType.Success,ToastrPosition.TopLeft)
+      localStorage.setItem('accessToken',tokenResponse.token.accessToken)
+    }
     callBackFunction();
   }
 }
