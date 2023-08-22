84e0a3a68d7eea26f92cd85e2039e1b78675c148
huseyin corakli <huseyincorakli@icloud.com>
2023-08-22 20:28:49 +0300

    Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,35 +3,36 @@ import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './admin/layout/layout.component';
 import { DashboardComponent } from './admin/components/dashboard/dashboard.component';
 import { HomeComponent } from './ui/components/home/home.component';
+import { AuthGuard } from './guards/common/auth.guard';
 
 const routes: Routes = [
   {
     path: 'admin',
     component: LayoutComponent,
     children: [
-      { path: '', component: DashboardComponent },
+      { path: '', component: DashboardComponent ,canActivate:[AuthGuard]},
       {
         path: 'customers',
         loadChildren: () =>
           import('./admin/components/customers/customers.module').then(
             (module) => module.CustomersModule
-          ),
+          ),canActivate:[AuthGuard]
       },
       {
         path: 'products',
         loadChildren: () =>
           import('./admin/components/products/products.module').then(
             (module) => module.ProductsModule
-          ),
+          ),canActivate:[AuthGuard]
       },
       {
         path: 'orders',
         loadChildren: () =>
           import('./admin/components/orders/orders.module').then(
             (module) => module.OrdersModule
-          ),
+          ),canActivate:[AuthGuard]
       },
-    ],
+    ],canActivate:[AuthGuard]
   },
   { path: '', component: HomeComponent },
   {
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { HttpClientModule } from '@angular/common/http';
+import { JwtModule } from '@auth0/angular-jwt';
 
 
 
@@ -23,7 +24,13 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     NgxSpinnerModule,
-    HttpClientModule
+    HttpClientModule,
+    JwtModule.forRoot({
+      config:{
+        tokenGetter:()=>localStorage.getItem('accessToken'),
+        allowedDomains:['localhost:7229']
+      }
+    })
   ],
   providers: [{
     provide:'baseUrl',useValue:'https://localhost:7229/api',multi:true
